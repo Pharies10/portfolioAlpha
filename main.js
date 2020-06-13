b930cf21644f86bf85d211a3894837b77ccf49f0
pharies10 <jackpharies@gmail.com>
2020-06-13 10:38:01 -0700

    added the window back button feature, time to test on browser--> need to fill out home page and test the back button usability on all browsers

--- main.js
+++ main.js
@@ -7,18 +7,22 @@
 
 */
 
-var directoryButtons = [{ text: "Home", call: "red"},
-                        { text: "About", call: "blue"},
-                        { text: "Portfolio", call: "green"},
-                        { text: "Resume", call: "yellow"},
-                        { text: "Resources", call: "white"}]
+var directoryButtons = [{ text: "Home", call: 0},
+                        { text: "About", call: 1},
+                        { text: "Portfolio", call: 2},
+                        { text: "Resume", call: 3},
+                        { text: "Resources", call: 4}]
 
 
 var currentWindow = directoryButtons[0].text
+window.history.pushState({ 'page_id': 5, 'user_id': 5 }, "Start")
+
+var gate = true
 
 var main = function()
 {
-    
+        
+       
     
        d3.select(".button_list")
         .selectAll("li")
@@ -34,13 +38,37 @@ var main = function()
            d3.select("#"+currentWindow).style("visibility", "hidden")
            d3.select("#"+d.text).style("visibility", "visible")
            currentWindow = d.text;
+           var state = { 'page_id': d.call, 'user_id': 5 }
+           window.history.pushState(state, d.text)
+           gate = false
            
            
            
        })
     
     
-    
+      window.addEventListener("popstate", function(d) {
+                                
+                               
+                                if (gate == true){
+                                    window.history.back()
+                                    
+                                    
+                                }
+                                if (d.state.page_id == 5) {
+                                    
+                                    callWindow(0)
+                                    gate = true
+                                    
+                                    
+                                    
+                                }
+                                else {
+                                    callWindow(d.state.page_id)
+                                }
+          
+          
+                                })
     
     
 
@@ -48,6 +76,18 @@ var main = function()
     
     
 }
+
+var callWindow = function(page) {
+    d3.select("#"+currentWindow).style("visibility", "hidden")
+    d3.select("#"+directoryButtons[page].text).style("visibility", "visible")
+    currentWindow = directoryButtons[page].text;
+    
+    
+    
+    
+    
+}
+
 
 var about_button = function() {
     d3.select("#"+currentWindow).style("visibility", "hidden")
